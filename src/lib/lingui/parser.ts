492615dda95a1950b68223fa336ce1b22424742e
Serchinastico <github@serchinastico.com>
2025-01-29 12:11:00 +0100

    feat: accept all lingui config files by using their own conf library

--- src/lib/lingui/parser.ts
+++ src/lib/lingui/parser.ts
@@ -1,6 +1,6 @@
-import {Ast} from '@/lib/ast/ast.js'
 import {invariant} from '@/lib/command/invariant.js'
-import {ArrayExpression, Literal, ObjectExpression, Property, VariableDeclaration, VariableDeclarator} from 'acorn'
+import {getConfig} from '@lingui/conf'
+import {combinatorial} from '@madeja-studio/cepillo'
 
 /**
  * Utility class for parsing and processing Lingui configuration files.
@@ -11,45 +11,12 @@ export class ConfigParser {
   constructor(private projectDir: string) {}
 
   async parse(configFilePath: string) {
-    const ast = await Ast.fromFile(configFilePath)
+    const config = getConfig({configPath: configFilePath})
 
-    const exports = ast
-      .filterByType('ExportNamedDeclaration')
-      .get<VariableDeclaration>('declaration')
-      .get<VariableDeclarator>('declarations')
+    invariant(config.format === 'po', 'unknown_catalog_file_format')
 
-    const locales = exports
-      .filter((node) => node.id?.type === 'Identifier' && node.id.name === 'locales')
-      .get<ArrayExpression>('init')
-      .get<Literal>('elements')
-      .get('value')
-
-    const catalogs = exports
-      .filter((node) => node.id?.type === 'Identifier' && node.id.name === 'catalogs')
-      .get<ArrayExpression>('init')
-      .get<ObjectExpression>('elements')
-      .get<Property>('properties')
-      .filter((node) => node.key.type === 'Identifier' && node.key.name === 'path')
-      .get<Literal>('value')
-      .get('value')
-
-    const formats = exports
-      .filter((node) => node.id?.type === 'Identifier' && node.id.name === 'format')
-      .get<Literal>('init')
-      .get('value')
-
-    const format = formats[0]
-    invariant(format === 'po', 'unknown_catalog_file_format')
-
-    const catalogFiles: string[] = []
-
-    for (const locale of locales) {
-      for (const catalog of catalogs) {
-        const resolvedPath = catalog.replace('<rootDir>', this.projectDir).replace('{locale}', locale)
-        catalogFiles.push(`${resolvedPath}.${format}`)
-      }
-    }
-
-    return catalogFiles
+    return combinatorial(config.locales, config.catalogs ?? []).flatMap(
+      ([locale, catalog]) => `${catalog.path.replace('{locale}', locale)}.${config.format}`,
+    )
   }
 }
